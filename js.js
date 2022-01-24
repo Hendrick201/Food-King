99491b0ef1f896cddea4c50e45e10b8bfa0bba73
Hendrick201 <81498960+Hendrick201@users.noreply.github.com>
2022-01-24 02:52:41 -0300

    Add files via upload

--- js.js
+++ js.js
@@ -29,4 +29,53 @@ function BorderAnimationOut()
         document.getElementsByClassName("CardápioBorder")[currentId].style.width = "0%"; 
 }
 
+setInterval(MenuAnimation,1)
+var FirstAnimationStart = true;
+var FirstAnimationEnds = false;
+var SecondAnimationEnds = false;
+var CanAnimate = true;
+function MenuAnimation()
+{
+        if(CanAnimate == true)
+        {
+                document.getElementById("SecondMenuText").style.animation = "FadeIn 2s";
+                document.getElementById("SecondMenuText").style.animationDelay = "0.5s";
+                setInterval(MenuAnimation2,1000)
+               
+          if(FirstAnimationEnds == true)
+                {
+                document.getElementById("MainMenuText").style.animation = "FadeIn 2s";
+                document.getElementById("MainMenuText").style.animationDelay = "0.2s";
+                setInterval(MenuAnimation1,500)
+                }
+         if(SecondAnimationEnds == true)
+                {
+                document.getElementById("MenuText").style.animation = "FadeIn 2s";
+                document.getElementById("MenuText").style.animationDelay = "0.2s";
+                setInterval(MenuAnimation3,500)
+               
+                AnimationEnds = true;
+                }
+        }
+              
+
+}
+function MenuAnimation1()
+{
+        document.getElementById("MainMenuText").style.opacity = "100%";
+        SecondAnimationEnds = true;
+        
+}
+function MenuAnimation2()
+{
+        document.getElementById("SecondMenuText").style.opacity = "100%";
+        FirstAnimationEnds = true;
+}
+function MenuAnimation3()
+{
+        document.getElementById("MenuText").style.opacity = "100%";
+        CanAnimate = false;
+}
+
+
 
