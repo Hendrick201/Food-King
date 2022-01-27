27f187e3e0b541dd3d31dbbb2aa271aee8b87abd
Hendrick201 <81498960+Hendrick201@users.noreply.github.com>
2022-01-27 01:16:52 -0300

    Add files via upload

--- js.js
+++ js.js
@@ -76,6 +76,102 @@ function MenuAnimation3()
         document.getElementById("MenuText").style.opacity = "100%";
         CanAnimate = false;
 }
+function MenuFixedAnimationHidden()
+{
+        document.getElementById("MenuContainer").style.animation = "MenuAnimHidden 0.8s"
+        document.getElementById("MenuContainer").style.top = "-120px" 
+        document.getElementById("MenuContainer").style.backgroundColor = "#000000bd"
+}
+function MenuFixedAnimationShow()
+{
+        document.getElementById("MenuContainer").style.animation = "MenuAnimShow 1s"
+        document.getElementById("MenuContainer").style.top = "0%"   
+        document.getElementById("MenuContainer").style.backgroundColor = "#000000bd" 
+}
+function BottomMenuFixedAnimationHidden()
+{
+        document.getElementById("FooterFixed").style.animation = "BottomMenuAnimHidden 0.8s"
+        document.getElementById("FooterFixed").style.bottom = "-40px" 
+}
+function BottomMenuFixedAnimationShow()
+{
+        document.getElementById("FooterFixed").style.animation = "BottomMenuAnimShow 1s"
+        document.getElementById("FooterFixed").style.bottom = "0%" 
+}
+function backgroundNone()
+{
+        document.getElementById("MenuContainer").style.backgroundColor = ""     
+}
+var clickCount = 0;
+var canClick = true;
+var canResetClick = false;
+function leftClickSlide()
+{
+ clickCount ++;
+}
+function rightClickSlide()
+{
+ clickCount -= 2;
+}
+function AlSlide()
+{
+        clickCount ++;
+}
+function clickReset()
+{
+        canClick = true;
+        canResetClick = false;
+}
+function ButtonOn()
+{
+        document.getElementById("RightButton").disabled = false;
+        document.getElementById("LeftButton").disabled = false;
+}
+setInterval(SlideImagesController, 0);
+// setInterval(AlSlide, 3000); ///passa o slide sozinho 
+function SlideImagesController()
+{
+                if(clickCount ==1)
+                {
+                        document.getElementById("Slider").style.animation = "SlideImagesShow 3s"
+                        document.getElementById("Slider").style.left =  "-100%" 
+                        document.getElementById("RightButton").disabled = true;
+                        document.getElementById("LeftButton").disabled = true;
+                        setInterval(ButtonOn,2999);
+                }
+                if(clickCount == -1)
+                {
+                        document.getElementById("Slider").style.animation = "SlideImagesHidden 3s"
+                        document.getElementById("Slider").style.left =  "0%" 
+                        document.getElementById("RightButton").disabled = true;
+                        document.getElementById("LeftButton").disabled = true;
+                        setInterval(ButtonOn,2999);
+                }              
+}
+
+var lastScrollTop = 0;
+window.addEventListener("scroll", function()
+{
+   var st = window.pageYOffset || document.documentElement.scrollTop;
+   if (st > lastScrollTop)
+   {
+    //downscroll code
+    MenuFixedAnimationHidden();
+    BottomMenuFixedAnimationShow();
+   } 
+   else 
+   {
+           //upscroll code
+           MenuFixedAnimationShow();
+           BottomMenuFixedAnimationHidden();
+   }
+   if(st == 0)
+   {
+           //upscroll in top page
+           backgroundNone();
+   }
+   lastScrollTop = st <= 0 ? 0 : st;
+}, false);
 
 
 
